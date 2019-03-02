5c89cffad186a8ba00f2b2d20548467e211e3181
Eric Ramseur <ramseur@anant.us>
2019-03-02 18:50:14 -0500

    Create Install_Cosmos_Emu.ps1
    commit of ps1 file

---
+++ src/Foundation/Install_Cosmos_Emu.ps1
@@ -0,0 +1,3 @@
+$client = new-object System.Net.WebClient
+$client.DownloadFile("https://cosmosdbportalstorage.azureedge.net/emulator/2019_02_28_2.2.1-2b4af9b3/azure-cosmosdb-emulator-2.2.1-2b4af9b3.msi","C:\Sitecore\azure-cosmosdb-emulator-2.2.1-2b4af9b3.msi")
+$exitCode = (Start-Process "msiexec.exe" -ArgumentList "/i C:\Sitecore\azure-cosmosdb-emulator-2.2.1-2b4af9b3.msi","/qb!" -wait -passthru).ExitCode
